e772a0f68aa071ace39f2ff44e51f9930ff897e9
tashiro <andre_seichi@hotmail.com>
2025-01-15 17:30:06 -0300

    adicionado input do usuário

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,30 @@
-import { Marqo } from "./Marqo";
 import { RAG } from "./Rag";
+import readline from "node:readline";
 
 const main = async (): Promise<void> => {
   const config = {
     ollama: {
       host: "http://localhost:11434",
-      model: "llama3.2",
-    },
-    marqo: {
-      host: "http://localhost:8882",
-      index: "documentos",
+      model: "llama3.1",
     },
     redisUrl: "redis://localhost:6379",
   };
 
-  const marqo = new Marqo(config.marqo);
   const rag = new RAG(config.ollama, config.redisUrl);
 
   try {
-    const userPrompt = "carne";
-    // const response = await rag.generateEmbedding(userPrompt);
-
-    // console.log("Resposta:", response);
-    // file mignon, bolo de chocolate, salada de batata com ovo
-
-    const receita = {
-      title: "salada de batata com ovo",
-      description: `
-      Ingredientes:
-        6 batatas cozidas com casca
-        4 ovos cozidos
-        1 cebola-roxa picada
-        50 gramas de azeitonas verdes
-        Suco de limão a gosto
-        Azeite de oliva a gosto
-        Sal a gosto
-        Pimenta-do-reino a gosto
-        Ervas picadas a gosto
-      Modo de preparo:
-        Descasque e corte as batatas e os ovos em cubos.
-        Disponha em uma tigela ou saladeira, junte a cebola e a azeitona e reserve.
-        Em um pote separado, faça um molho com os demais ingredientes e regue a salada.
-        Misture delicadamente e leve para gelar antes de servir decorada a gosto.
-`,
-    };
-
-    const document1 = {
-      id: "1",
-      content:
-        "salada de batata com ovo.\nIngredientes: 6 batatas cozidas com casca, 4 ovos cozidos, 1 cebola-roxa picada, 50 gramas de azeitonas verdes, Suco de limão a gosto, Azeite de oliva a gosto, Sal a gosto, Pimenta-do-reino a gosto, Ervas picadas a gosto.\nModo de preparo: Descasque e corte as batatas e os ovos em cubos. Disponha em uma tigela ou saladeira, junte a cebola e a azeitona e reserve. Em um pote separado, faça um molho com os demais ingredientes e regue a salada. Misture delicadamente e leve para gelar antes de servir decorada a gosto.",
-    };
-
-    // const response = await marqo.search(userPrompt);
-
     await rag.init();
 
-    await rag.addDocument(document1);
+    const userInput = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-    const response = rag.search("batata");
-
-    console.log("Resposta:", JSON.stringify(response, null, 2));
+    userInput.question("Qual sua dúvida com suas finanças?", async (input) => {
+      userInput.close();
+      const generatedResponse = await rag.generateResponse(input);
+      console.log(generatedResponse);
+    });
   } catch (error) {
     console.error("Erro:", error);
   } finally {
